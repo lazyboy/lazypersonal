88af7031c761ab1014080aa8c85110727bb1b601
Istiaque Ahmed <lazyboybd@gmail.com>
2011-12-13 02:51:32 -0500

    added file traversing, debug flag, main method

--- wxtest1/app.py
+++ wxtest1/app.py
@@ -1,26 +1,36 @@
-# Comment
 from wrapper import GuiWrapper
 import wx
 from controller import Controller
+import sys
+import config
 
 c = None
 def initFactory():
   global c
   c = Controller()
 
-initFactory()
+def main():
+  # Make them robust
+  if len(sys.argv) > 1 and sys.argv[1] == '--DEBUG':
+    config.Config.DEBUG = True
 
-app = wx.App()
-frame = GuiWrapper(None, c)
+  initFactory()
 
-frame.listCtrl.InsertColumn(0, 'hi0')
-frame.listCtrl.InsertColumn(1, 'hi1')
-frame.listCtrl.InsertColumn(2, 'hi2')
+  app = wx.App()
+  frame = GuiWrapper(None, c)
 
-pos = frame.listCtrl.InsertStringItem(0, 'hello')
-frame.listCtrl.SetStringItem(pos, 1, 'world')
-frame.listCtrl.SetStringItem(pos, 2, '!')
+  frame.listCtrl.InsertColumn(0, 'hi0')
+  frame.listCtrl.InsertColumn(1, 'hi1')
+  frame.listCtrl.InsertColumn(2, 'hi2')
 
-frame.Show()
-app.MainLoop()
+  pos = frame.listCtrl.InsertStringItem(0, 'hello')
+  frame.listCtrl.SetStringItem(pos, 1, 'world')
+  frame.listCtrl.SetStringItem(pos, 2, '!')
+
+  frame.Show()
+  app.MainLoop()
+  return
+
+if __name__ == '__main__':
+  main()
 
--- wxtest1/controller.py
+++ wxtest1/controller.py
@@ -1,15 +1,36 @@
+import os
+import config
+import structs
+
+global DEBUG
+
 class Controller:
   def __init__(self):
     #self.fileSearcher = FileSearcher()
+
+    # Load config
     pass
 
   def onSearchItem(self, event, query):
-    '''
-    items = self.fileSearcher.getMatchedItems(query)
-    idx = 0
-    for item in items:
-      print idx, ':', item
-      idx = idx + 1
-    '''
     print 'query is', query
+    if config.Config.DEBUG:
+      if '' == query:
+        query = '/home/lazyboy/media'
+
+    if not os.path.exists(query):
+      print 'Path %s does not exist' % query
+      return
+
+    idx = 1
+    for root, dirs, files in os.walk(query):
+      for name in files:
+        fullPath = os.path.join(root, name)
+        try:
+          size = os.path.getsize(fullPath)
+        except os.error as e:
+          print 'Error:', e
+          size = -1
+        print '%04d: %d bytes %s' % (idx, size, fullPath)
+        idx = idx + 1
+    return
 
