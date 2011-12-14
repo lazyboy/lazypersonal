aaf426ea276af4a48ba0cb1bf04fec8806c17265
Istiaque Ahmed <lazyboybd@gmail.com>
2011-12-14 00:51:08 -0500

    add int reading and writing config file

--- deleteme/config_parser.py
+++ deleteme/config_parser.py
@@ -4,13 +4,25 @@ import os
 def main():
   config = ConfigParser.SafeConfigParser()
   configPath = os.path.expanduser('~/.douchebag')
-  config.read(configPath)
 
-  ret = config.has_option('global', 'max-filesize')
-  print 'hasth option: ', ret
+  try:
+    config.read(configPath)
+  except:
+    print '...Exception reading'
+
+  if not config.has_section('global'):
+    config.add_section('global')
+
+  if not config.has_option('global', 'max-filesize'):
+    config.set('global', 'max-filesize', str(100 * 1000 * 1000))
+
 
   ivalue = config.getint('global', 'max-filesize')
-  print 'int value: ', ivalue
+  print 'int value:', ivalue
+
+  # Write config to file
+  with open(configPath, 'wb') as configFile:
+    config.write(configFile)
 
   return
 
