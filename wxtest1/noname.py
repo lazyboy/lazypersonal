3c61e61ee76d3975cdcb008f3756bb3f9bb1ea1f
lazyboy <lazyboybd@gmail.com>
2011-12-16 02:22:39 -0500

    revert all changes to generated gui(noname.py), do them in inherited class instead

--- wxtest1/noname.py
+++ wxtest1/noname.py
@@ -6,10 +6,8 @@
 ##
 ## PLEASE DO "NOT" EDIT THIS FILE!
 ###########################################################################
-## WARNING: EDITED ALREADY :P
 
 import wx
-import sys
 
 ###########################################################################
 ## Class GuiFrame
@@ -19,7 +17,7 @@ class GuiFrame ( wx.Frame ):
   KEYCODE_J = 106
   KEYCODE_K = 107
   
-  def __init__( self, parent):
+  def __init__( self, parent ):
     wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,346 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
     
     self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
@@ -55,75 +53,13 @@ class GuiFrame ( wx.Frame ):
     self.textCtrlSearch.Bind( wx.EVT_TEXT_ENTER, self.OnTextSearch )
     self.buttonBrowse.Bind( wx.EVT_BUTTON, self.OnBrowseClick )
     self.buttonGo.Bind( wx.EVT_BUTTON, self.OnGoClick )
-
-    self.listCtrl.Bind(wx.EVT_CHAR, self.onChar)
-
   
   def __del__( self ):
     pass
-
-  def setBridge(self, b):
-    self.bridge = b
-
-  def onCustomEvent(self, event, args):
-    if 'showListItems' == event:
-      self.listCtrl.ClearAll()
-      self.listCtrl.DeleteAllColumns()
-
-      # TODO(I.A.): Proper column size.
-      self.listCtrl.InsertColumn(0, 'idx')
-      self.listCtrl.InsertColumn(1, 'name')
-      self.listCtrl.InsertColumn(2, 'size')
-      print 'Total showListItems: ', len(args)
-
-      idx = 1
-      for fileinfo in args:
-        pos = self.listCtrl.InsertStringItem(sys.maxint, str(idx))
-        self.listCtrl.SetStringItem(pos, 1, fileinfo.name)
-        self.listCtrl.SetStringItem(pos, 2, str(fileinfo.size))
-        idx = idx + 1
-    else:
-      print 'Unknown event in Frame:', event
-    return
-
-  def onChar(self, event):
-    #print 'onChar', event.GetKeyCode()
-    keyCode = event.GetKeyCode()
-    if keyCode == GuiFrame.KEYCODE_J: # j
-      #print 'go Down'
-      self.moveSelection(isUp = False)
-    elif keyCode == GuiFrame.KEYCODE_K:
-      #print 'go Up'
-      self.moveSelection(isUp = True)
-    else:
-      # Tip: be sure to call event.Skip() for events that you don't process in
-      # key event function, otherwise menu shortcuts may cease to work under
-      # Windows.
-      event.Skip()
-
-  def moveSelection(self, isUp = False):
-    idx = self.listCtrl.GetFirstSelected()
-    #print 'GetFirstSelected:', idx
-    nidx = 0
-    n = self.listCtrl.GetItemCount()
-    if idx != -1:
-      nidx = (idx - 1) if isUp else (idx + 1)
-      if nidx == -1:
-        nidx = 0
-      elif nidx >= n:
-        nidx = n - 1
-      self.listCtrl.Select(idx, on = False)
-    #print 'nextIdx:', nidx
-    if nidx != -1:
-      self.listCtrl.Select(nidx, on = True)
-      self.listCtrl.Focus(nidx)
   
   
   # Virtual event handlers, overide them in your derived class
   def OnTextSearch( self, event ):
-    print 'OnTextSearch'
-    value = self.textCtrlSearch.GetValue()
-    self.bridge.sendToController('onSearchItem', value)
     event.Skip()
   
   def OnBrowseClick( self, event ):
--- wxtest1/wrapper.py
+++ wxtest1/wrapper.py
@@ -1,12 +1,83 @@
 from noname import GuiFrame
-import wx
+import sys, wx
 import controller
 
 class GuiWrapper (GuiFrame):
+  KEYCODE_J = 106
+  KEYCODE_K = 107
+
   def __init__(self, parent):
     GuiFrame.__init__(self, parent)
+    # So we can track J/K
+    self.listCtrl.Bind(wx.EVT_CHAR, self.onChar)
 
     # Set focus on text box.
     self.textCtrlSearch.SetFocus()
   
+  def setBridge(self, b):
+    self.bridge = b
+
+  def onCustomEvent(self, event, args):
+    if 'showListItems' == event:
+      self.listCtrl.ClearAll()
+      self.listCtrl.DeleteAllColumns()
+
+      # TODO(I.A.): Proper column size.
+      self.listCtrl.InsertColumn(0, 'idx')
+      self.listCtrl.InsertColumn(1, 'name')
+      self.listCtrl.InsertColumn(2, 'size')
+      print 'Total showListItems: ', len(args)
+
+      idx = 1
+      for fileinfo in args:
+        pos = self.listCtrl.InsertStringItem(sys.maxint, str(idx))
+        self.listCtrl.SetStringItem(pos, 1, fileinfo.name)
+        self.listCtrl.SetStringItem(pos, 2, str(fileinfo.size))
+        idx = idx + 1
+    else:
+      print 'Unknown event in Frame:', event
+    return
+
+  def onChar(self, event):
+    #print 'onChar', event.GetKeyCode()
+    keyCode = event.GetKeyCode()
+    if keyCode == GuiWrapper.KEYCODE_J: # j
+      #print 'go Down'
+      self.moveSelection(isUp = False)
+    elif keyCode == GuiWrapper.KEYCODE_K:
+      #print 'go Up'
+      self.moveSelection(isUp = True)
+    else:
+      # Tip: be sure to call event.Skip() for events that you don't process in
+      # key event function, otherwise menu shortcuts may cease to work under
+      # Windows.
+      event.Skip()
+
+  def moveSelection(self, isUp = False):
+    idx = self.listCtrl.GetFirstSelected()
+    #print 'GetFirstSelected:', idx
+    nidx = 0
+    n = self.listCtrl.GetItemCount()
+    if idx != -1:
+      nidx = (idx - 1) if isUp else (idx + 1)
+      if nidx == -1:
+        nidx = 0
+      elif nidx >= n:
+        nidx = n - 1
+      self.listCtrl.Select(idx, on = False)
+    #print 'nextIdx:', nidx
+    if nidx != -1:
+      self.listCtrl.Select(nidx, on = True)
+      self.listCtrl.Focus(nidx)
+
+  ############# Added from wxFormBuilder ################
+  def OnTextSearch(self, event):
+    value = self.textCtrlSearch.GetValue()
+    self.bridge.sendToController('onSearchItem', value)
+
+  def OnBrowseClick( self, event ):
+    event.Skip()
+
+  def OnGoClick( self, event ):
+    event.Skip()
 
