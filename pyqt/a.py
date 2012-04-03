059552ad2fe7b6f0d8be5ba2215b4d2db319dbb3
unknown <lazyboy@lazyboy-dm1.(none)>
2012-04-03 07:00:21 +0100

    bidirectional connection within js and py

--- pyqt/a.py
+++ pyqt/a.py
@@ -8,15 +8,41 @@ from PyQt4.QtWebKit import *
 
 from PyQt4 import QtCore
 
-app = QApplication(sys.argv)
+class TWebView(QWebView):
+  def __init__(self, parent = None):
+    QWebView.__init__(self, parent)
+    self._jsBridge = JsBridge(self, self)
+    self.connect(self.page().mainFrame(),
+        SIGNAL('javaScriptWindowObjectCleared()'), self.getJsBridge)
 
-web = QWebView()
+  def getJsBridge(self):
+    self.page().mainFrame().addToJavaScriptWindowObject('_abacus', self._jsBridge)
+
+  def testDownStream(self):
+    self.page().mainFrame().evaluateJavaScript(
+        '__lazy_py_void_connection("Hello from python2")')
+    return
+
+class JsBridge(QtCore.QObject):
+  def __init__(self, parent, webview):
+    QtCore.QObject.__init__(self, parent)
+    self._webview = webview
+
+  # QString receive(QString) {}
+  @QtCore.pyqtSlot(str, result=str)
+  def receive(self, msg):
+    print('[py] %s' % msg)
+    self._webview.testDownStream()
+    return 'msg-returned-from-upstream'
+
+
+app = QApplication(sys.argv)
+web = TWebView()
 #web.load(QUrl("http://google.pl"))
 
 # 1. Opens from py path
 baseq = QApplication.applicationDirPath();
 qq = QUrl.fromLocalFile(QDir.toNativeSeparators(baseq + '/lazy.html'))
-
 print('hello %d' % qq.isLocalFile())
 
 # 2. Opens Current dir/path             	
@@ -30,6 +56,14 @@ web.load(qq)
 # 3. Opening raw string as html.
 #html = '<body>raw write</body>'
 #web.setHtml(html, QtCore.QUrl('qrc:/')) #works
+
+#web.show()
+
+## 1. Downstream test.
+# See TWebView.testDownStream.
+
+## 2. Upstream test.
+# See JsBridge.receive
 
 web.show()
 
--- pyqt/main.js
+++ pyqt/main.js
@@ -2,10 +2,41 @@ var lazy = {};
 
 lazy.$ = function(id) { return document.getElementById(id); };
 
+lazy.RENDER_DIV_ID = 'render-div-id';
+
+lazy.render = function() {
+  var div = document.createElement('div');
+  div.id = lazy.RENDER_DIV_ID;
+  document.body.appendChild(div);
+};
+
 lazy.demo = function() {
+  lazy.render();
+
   var el = lazy.$('export-container');
   if (!el) { alert('export element not found'); return; }
   el.innerText = 'Rendered by main.js';
+
+  // Test upstream message passing.
+  var s = 'From downstream js';
+  var slot = window['_abacus'] || _abacus;
+  if (!slot) {
+    alert('No connection to upstream');
+  } else {
+    var retValue = slot.receive(s);
+    lazy.demoRender('[js]: sent to py, returned: ' + retValue);
+  }
+
 };
 
+lazy.demoRender = function(str) {
+  lazy.$(lazy.RENDER_DIV_ID).innerHTML += '<div>' + str + '</div>';
+};
+
+// Yet to pass py param.
+lazy.py_void_connection = function(str) {
+  lazy.demoRender('[js]: ' + str);
+};
+
+window['__lazy_py_void_connection'] = lazy.py_void_connection;
 lazy.demo();
