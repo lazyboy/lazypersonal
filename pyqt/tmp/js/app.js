4dfc7938461d84466f09e99407328ff50cc28f87
lazyboy <lazyboybd@gmail.com>
2012-04-08 01:29:33 +0100

    app.start calls init if necessary

--- pyqt/tmp/js/app.js
+++ pyqt/tmp/js/app.js
@@ -2,15 +2,20 @@ lazy = lazy || {};
 
 lazy.App = function() {
   this.views_ = {};
+  this.initCalled_ = false;
 };
 
 lazy.App.prototype.init = function() {
+  this.initCalled_ = true;
   // Bulk add view.
   lazy.core.has('lazy.views.Bulk');
   this.views_['bulk'] = new lazy.views.Bulk('bulk');
 };
 
 lazy.App.prototype.start = function() {
+  if (!this.initCalled_) {
+    this.init();
+  }
   var view = this.views_['bulk'];
   if (!view) {
     window.console.log('app cannot find view');
