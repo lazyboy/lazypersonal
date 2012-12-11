03c13756f3ea13bfb504f1fef3005d1102181cef
lazyboy <lazyboybd@gmail.com>
2012-12-11 15:45:52 -0500

    move speech app

--- chromium/test_cases/platform_apps/speech/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  window.console.log('onload');
-  var el = document.getElementById('sid');
-  if (!el) { window.console.log('element not found'); return; }
-  el.onspeechchange = function() { window.console.log('**** onspeechchange'); }
-};
